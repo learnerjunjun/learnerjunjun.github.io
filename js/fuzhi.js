6568a89a8db63b222a3c3df3f0c539bdf3bf9067
learnerjunjun <vyuan217@gmail.com>
2025-08-11 16:49:31 +0000

    fix: update GitHub Actions workflow - remove hardcoded token, update actions to latest versions Updated By Github Actions

--- js/fuzhi.js
+++ js/fuzhi.js
@@ -1,9 +1,1 @@
-/** 监听 copy 事件 */
-document.addEventListener("copy",function(e){
-    // 复制的内容
-    btf.snackbarShow('复制成功👻', false, 3000)
-  })
-// 检测按键
-window.onkeydown = function (e) {if (e.keyCode === 123) {btf.snackbarShow('开发者模式已打开，请遵循 GPL 协议', false, 3000)
-  }
-}  
+document.addEventListener("copy",(function(n){btf.snackbarShow("复制成功👻",!1,3e3)})),window.onkeydown=function(n){123===n.keyCode&&btf.snackbarShow("开发者模式已打开，请遵循 GPL 协议",!1,3e3)};
